e7c56d707a0d19cbc3b644c237e36ba53510972d
JaRose <newson.jaluan16@gmail.com>
2025-02-27 14:47:02 -0600

    fixed quotes to backticks

--- drum.js
+++ drum.js
@@ -2,13 +2,19 @@
 //going to add an event listener to the drum buttons
 //when the drum button is clicked, it will play the sound   
 //we want them in their own function 
+function removeTransition(e) { //we need this because we want to remove the playing class after the transition ends so it wont get out of sync, since we have transition in our css file.
+    if (e.propertyName !== 'transform') return; // skip it if it's not a transform 
+    // console.log the e.propertyName function to make sure it works 
+    e.target.classList.remove('playing'); //remove the playing class from the key, we need to use this because we are inside the function and we are referring to the key that is being played
+}
+
 function playSound(e){
-    const audio = document.querySelector('audio[data-key="${e.keyCode}"]'); //this will select the audio element that has the data-key attribute that matches the keycode of the key that was pressed
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); //this will select the audio element that has the data-key attribute that matches the keycode of the key that was pressed
     //why are we using the backticks here?
     //we are using backticks because we are using a template string
     //template strings allow us to embed expressions in strings
     //we are using the ${} syntax to embed the expression
-    const key = document.querySelector(' div[data-key="${e.keyCode}"]'); 
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`); 
     if (!audio) return; //stop the function from running all together
    
     key.classList.add('playing'); //add the playing class to the key
@@ -16,11 +22,6 @@ function playSound(e){
     audio.play(); //play the audio
 } 
 
-function removeTransform(e) {
-    if (e.propertyName !== 'transform') return; // skip it if it's not a transform 
-    // console.log the e.propertyName function to make sure it works 
-    e.target.classList.remove('playing'); //remove the playing class from the key, we need to use this because we are inside the function and we are referring to the key that is being played
-}
 const keys = Array.from(document.querySelectorAll('.key')); //// transition end event listener, we need this because we want to remove the playing class after the transition ends so it wont get out of sync, since we have transition in our css file. 
 keys.forEach(key=> key.addEventListener('transitionend', removeTransition)); //we need to code it this way to remove the transition from all the keys 
 
